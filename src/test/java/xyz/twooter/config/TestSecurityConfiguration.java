ae3a2928f9399bb3de3f7929fac840491e22ccff
dar-jeeling <o_o___t@pusan.ac.kr>
2025-04-13 23:10:41 +0900

    env: test 환경에서의 jwt 설정 추가

--- src/test/java/xyz/twooter/config/TestSecurityConfiguration.java
+++ src/test/java/xyz/twooter/config/TestSecurityConfiguration.java
@@ -2,13 +2,26 @@ package xyz.twooter.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import xyz.twooter.auth.infrastructure.jwt.JWTUtil;
+
 @TestConfiguration
 @EnableWebSecurity
 public class TestSecurityConfiguration {
+
+	private static final String TEST_JWT_SECRET =
+		"testSecretKeyForJWTGenerationInTestEnvironmentMustBeSecureLongEnoughToWorkWithHS256Algorithm";
+
+	@Bean
+	@Primary
+	public JWTUtil jwtUtil() {
+		return new JWTUtil(TEST_JWT_SECRET);
+	}
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
