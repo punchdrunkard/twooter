b811e80bd0e71be2a931f8f5e1393ae46cbe3346
punchdrunkard <o_o___t@pusan.ac.kr>
2025-05-19 15:44:07 +0900

    feat: `getMediaByPostId` 구현

--- src/main/java/xyz/twooter/media/domain/repository/MediaRepository.java
+++ src/main/java/xyz/twooter/media/domain/repository/MediaRepository.java
@@ -3,9 +3,13 @@ package xyz.twooter.media.domain.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import xyz.twooter.media.domain.Media;
 
 public interface MediaRepository extends JpaRepository<Media, Long> {
 	List<Media> findAllByIdIn(List<Long> ids);
+
+	@Query("SELECT m FROM Media m JOIN PostMedia pm ON m.id = pm.mediaId WHERE pm.postId = :postId")
+	List<Media> findMediaByPostId(Long postId);
 }
--- src/main/java/xyz/twooter/post/presentation/dto/response/MediaEntity.java
+++ src/main/java/xyz/twooter/post/presentation/dto/response/MediaEntity.java
@@ -3,11 +3,19 @@ package xyz.twooter.post.presentation.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import xyz.twooter.media.domain.Media;
 
 @Getter
 @Builder
 @AllArgsConstructor
 public class MediaEntity {
-	Long id;
-	String path;
+	private Long id;
+	private String path;
+
+	public static MediaEntity fromEntity(Media media) {
+		return MediaEntity.builder()
+			.id(media.getId())
+			.path(media.getPath())
+			.build();
+	}
 }
