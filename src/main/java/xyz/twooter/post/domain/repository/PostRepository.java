574bf2b84e3f0cf35bbd63e87394d50c5bec331f
punchdrunkard <o_o___t@pusan.ac.kr>
2025-05-28 17:14:40 +0900

    refactor: 필요없는 import 삭제

--- src/main/java/xyz/twooter/post/domain/repository/PostRepository.java
+++ src/main/java/xyz/twooter/post/domain/repository/PostRepository.java
@@ -2,8 +2,6 @@ package xyz.twooter.post.domain.repository;
 
 import java.util.Optional;
 
-import javax.swing.text.html.Option;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
